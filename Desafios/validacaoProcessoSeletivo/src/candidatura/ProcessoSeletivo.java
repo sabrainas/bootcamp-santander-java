479aff1ec507d8c32985b59ea7dfbef90efade2e
sabrainas <sazonass.m@gmail.com>
2023-09-06 21:59:33 -0300

    validação de processo seletivo

--- Desafios/validacaoProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
+++ Desafios/validacaoProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
@@ -1,13 +1,48 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
 		System.out.println("Processo seletivo");
+		String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
 		
-		selecaoCandidatos();
+		for(String candidato: candidatos) {
+			entrandoEmContato(candidato);
+		}
 	}
-	
+	static void entrandoEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu = false;
+		do {
+			atendeu = atender();
+			continuarTentando = !atendeu;
+			
+			if(continuarTentando) {
+				tentativasRealizadas++;
+			}else {
+				System.out.println("CONTATO REALIZADO COM SUCESSO");
+			}
+		}while(continuarTentando && tentativasRealizadas < 3);
+		
+		if(atendeu) {
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+		}else {
+			System.out.println("NAO CONSEGUIMOS CONTATO COM " + candidato + ", NUMERO MAXIMO DE TENTATIVAS ATINGIDAS");
+		}
+	}
+	//metodo auxiliar
+	static boolean atender() {
+		return new Random().nextInt(3) == 1;
+	}
+	static void imprimirSelecionados(){
+		String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+
+		for(String candidato: candidatos) {
+			System.out.println("O candidato selecionado foi: " + candidato);
+		}
+	}
 	static void selecaoCandidatos() {
 		String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
 		
@@ -40,5 +75,4 @@ public class ProcessoSeletivo {
 			System.out.println("ANALISAR OUTROS CANDIDATOS");
 		}
 	}	
-	
 };
