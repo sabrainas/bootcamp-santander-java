9e1ef49e682087dd97b6e4e317a519fcd6ffd0d2
sabrainas <sazonass.m@gmail.com>
2023-09-16 17:17:24 -0300

    aulas POO

---
+++ exemplos-pacotes/src/pessoa/Pessoa.java
@@ -0,0 +1,28 @@
+package pessoa;
+
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private String endereco;
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	//...
+	//setters de nome e cpf ?
+	// método construtor
+	// o nome deverá ser igual ao nome da classe
+	public Pessoa (String cpf, String nome) {
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+}
